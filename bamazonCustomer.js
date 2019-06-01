d5ddda7d43d5185a7188a71b05d1fe6ce56147fa
unknown <exumcodes@gmail.com>
2019-05-31 20:52:05 -0600

    initial commit and bare bones

---
+++ bamazonCustomer.js
@@ -0,0 +1,59 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
+
+function enterItemID() {
+  inquirer
+    .prompt([
+      {
+      name: "itemID",
+      type: "input",
+        message: "What is the ID number of item you would like to purchase?"
+      },
+      {
+      name: "itemID",
+      type: "input",
+        message: "How many units would you like to purchase of this item?"
+      }
+    ])
+    .then(function(answer) {
+      // when finished prompting, insert a new item into the db with that info
+      connection.query(
+        "INSERT INTO auctions SET ?",
+        {
+          item_name: answer.item,
+          category: answer.category,
+          starting_bid: answer.startingBid || 0,
+          highest_bid: answer.startingBid || 0
+        },
+        function(err) {
+          if (err) throw err;
+          console.log("Your auction was created successfully!");
+          // re-prompt the user for if they want to bid or post
+          start();
+        }
+      );
+    });
+}
+
